0523ae9747bba63f595fca15580dcc8d58cc910d
Zubair <zubair.cache@gmail.com>
2024-06-14 18:17:54 +0500

    chnaged styling

--- src/test.jsx
+++ src/test.jsx
@@ -66,3 +66,32 @@ return (
       </div>
     </div>
   );
+
+
+  // todo item
+
+  <div>
+			
+  <div className="relative flex gap-x-3">
+    
+    <div className="flex h-6 items-center">
+      
+      <input
+        type="checkbox"
+        name=""
+        checked={task.completed}
+        onChange={handleChange}
+        className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+      />
+    </div>
+    <div className="text-sm leading-6">
+      
+      <label htmlFor="comments" className="font-medium text-gray-900">
+        
+        {task.completed ? 'Done' : 'Pending'}
+      </label>
+    </div>
+  </div>
+  <p className={task.completed ? 'line-through' : ''}>{task.text}</p>
+  <button onClick={() => deleteTask(task.id)}>Delete</button>
+</div>
